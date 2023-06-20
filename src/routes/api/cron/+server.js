29025a27ce1b81dbc9e7328d18899a43d5245884
Lukas Klein <lukas.klein@telekom.de>
2023-06-20 21:42:29 +0200

    added cron job

--- src/routes/api/cron/+server.js
+++ src/routes/api/cron/+server.js
@@ -5,6 +5,7 @@ import nodemailer from 'nodemailer';
 
 // @ts-ignore
 export default async function GET(req, res) {
+    return new Response('Hello Cron!');
       // Get the currlent date in the needed format
       const current = new Date();
       current.setDate(current.getDate());
@@ -31,41 +32,41 @@ export default async function GET(req, res) {
         return;
       }
     
-      if (data.length === 0) {
-        const transporter = nodemailer.createTransport({
-          host: 'smtp.ethereal.email',
-          port: 587,
-          secure: false,
-          auth: {
-            user: 'lukasklein1604@outlook.de',
-            pass: PUBLIC_EMAIL_PASSWORD
-          }
-        });
+    //   if (data.length === 0) {
+    //     const transporter = nodemailer.createTransport({
+    //       host: 'smtp.ethereal.email',
+    //       port: 587,
+    //       secure: false,
+    //       auth: {
+    //         user: 'lukasklein1604@outlook.de',
+    //         pass: PUBLIC_EMAIL_PASSWORD
+    //       }
+    //     });
     
-        const message = {
-          from: 'PollenTracker <lukasklein1604@outlook.de>',
-          to: 'lukasklein1604@outlook.de',
-          subject: 'No changes today',
-          text: 'There have been no changes today',
-          html: '<p>There have been no changes today</p>'
-        };
+    //     const message = {
+    //       from: 'PollenTracker <lukasklein1604@outlook.de>',
+    //       to: 'lukasklein1604@outlook.de',
+    //       subject: 'No changes today',
+    //       text: 'There have been no changes today',
+    //       html: '<p>There have been no changes today</p>'
+    //     };
     
-        transporter.sendMail(message, (error) => {
-          if (error) {
-            console.log(error);
-            res.status(500).json({ error: 'Internal server error' });
-            return new Response('error!');
-          }
+    //     transporter.sendMail(message, (error) => {
+    //       if (error) {
+    //         console.log(error);
+    //         res.status(500).json({ error: 'Internal server error' });
+    //         return new Response('error!');
+    //       }
           
-          res.status(200).json({ message: 'No changes today, email sent' });
-        });
+    //       res.status(200).json({ message: 'No changes today, email sent' });
+    //     });
         
-        return new Response('Hello Cron!');
-      }
-      else{
-        // If there are results, there has been a change on the current day
-        res.status(200).json({ message: `Changes found: ${JSON.stringify(data)}` });
-        }
+    //     return new Response('Hello Cron!');
+    //   }
+    //   else{
+    //     // If there are results, there has been a change on the current day
+    //     res.status(200).json({ message: `Changes found: ${JSON.stringify(data)}` });
+    //     }
 
         return new Response('Hello Cron!');
     }
